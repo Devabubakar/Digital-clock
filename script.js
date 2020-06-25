76526912d8a9bedce48a6aef40e41b568b997e6a
sultan254 <abubakarali3w@gmail.com>
2020-06-25 14:19:46 +0300

    Initial version

---
+++ script.js
@@ -0,0 +1,23 @@
+function setTime(){
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s= today.getSeconds();
+    var date = today.toDateString();
+
+    m=checkTime(m);
+    s=checkTime(s)
+
+    document.getElementById("words").innerHTML =
+    h + ":" + m + ":"+ s;
+    document.getElementById("d").innerHTML = 
+    date
+};
+
+function checkTime(i){
+    if (i<10){
+        i = "0"+i;
+    };
+    return i;
+  
+};
